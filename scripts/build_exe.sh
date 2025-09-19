508714d79841db7970525cf3227e47c50575f3c8
Riccardo Casula <riccardocasula@aridhia.net>
2025-09-19 15:47:41 +0100

    actions: Added a simplistic pipeline (#2)
    * actions: Added a simplistic pipeline
    
    * actions: Added tests in pipeline, refactored the exe release
    
    * actions: Push new release only on new tags

--- scripts/build_exe.sh
+++ scripts/build_exe.sh
@@ -3,3 +3,6 @@
 APP_NAME=fn-config-helper
 
 GOOS=windows GOARCH=amd64 go build -o artifacts/$APP_NAME.exe ./main.go
+
+zip -j artifacts/fn-config-helper-win-amd64.zip artifacts/fn-config-helper.exe README.md
+rm artifacts/fn-config-helper.exe
