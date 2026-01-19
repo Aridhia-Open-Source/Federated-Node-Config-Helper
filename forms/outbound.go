c9d6b43095bdc71d972bce80d90bcc55194d2eac
Riccardo Casula <riccardocasula@aridhia.net>
2026-01-19 11:13:05 +0000

    gh-additional-secret: Added separate form for repo trigger. Refactored the layout for outbound mode

--- forms/outbound.go
+++ forms/outbound.go
@@ -11,13 +11,27 @@ import (
 )
 
 var DeliveryOptions = []string{"none", "github", "other"}
+var dropDownDeliveryType = tview.NewDropDown()
 var OutboundSettingsForm = tview.NewForm().
 	AddCheckbox("Outbound mode", true, nil).
-	AddDropDown("Deliver to", DeliveryOptions, 0, HandleDeliveryOpts)
+	AddFormItem(dropDownDeliveryType)
 
 var createGHSecretButton = tview.NewButton("Create Secrets")
 
 func init() {
+	dropDownDeliveryType.SetLabel("Deliver to").
+		SetOptions(DeliveryOptions, nil).
+		SetCurrentOption(0).
+		SetSelectedFunc(HandleDeliveryOpts)
+	SameAsTrigger.SetChangedFunc(func(checked bool) {
+		_, deliveryIS := dropDownDeliveryType.GetCurrentOption()
+		if !checked && deliveryIS == DeliveryOptions[1] {
+			GhSecretsContainer.AddItem(GhDeliveryForm, 0, 1, false)
+		} else {
+			GhSecretsContainer.RemoveItem(GhDeliveryForm)
+		}
+	})
+
 	createGHSecretButton.SetSelectedFunc(func() {
 		client, _ := helpers.NewRealKubeClient()
 		secretName := gitHubSecretName.GetText()
@@ -31,10 +45,12 @@ func init() {
 				},
 			)
 		}
-		secretName = fmt.Sprintf("%s-%s", strings.ToLower(gitHubOrganization.GetText()), strings.ToLower(gitHubRepo.GetText()))
+
+		// Trigger secret
+		secretName = fmt.Sprintf("%s-%s", strings.ToLower(gitHubTriggerOrganization.GetText()), strings.ToLower(gitHubTriggerRepo.GetText()))
 		if secretName != "-" {
 			// Read the pem file
-			pemKey, err := os.ReadFile(gitHubPemFilePath.GetText())
+			pemKey, err := os.ReadFile(gitHubTriggerPemFilePath.GetText())
 			if err != nil {
 				components.ErrorBoard.SetText(err.Error())
 			} else {
@@ -44,9 +60,31 @@ func init() {
 					secretName,
 					map[string]string{
 						"key.pem":      string(pemKey),
-						"GH_CLIENT_ID": gitHubdeliveryClientId.GetText(),
+						"GH_CLIENT_ID": gitHubTriggerClientId.GetText(),
 					},
 				)
+			}
+		}
+		// If different, create the delivery one as well
+		_, deliveryIS := dropDownDeliveryType.GetCurrentOption()
+		if !SameAsTrigger.IsChecked() && deliveryIS == DeliveryOptions[1] {
+			secretName = fmt.Sprintf("%s-%s", strings.ToLower(gitHubOrganization.GetText()), strings.ToLower(gitHubRepo.GetText()))
+			if secretName != "-" {
+				// Read the pem file
+				pemKey, err := os.ReadFile(gitHubPemFilePath.GetText())
+				if err != nil {
+					components.ErrorBoard.SetText(err.Error())
+				} else {
+					components.ErrorBoard.SetText("")
+					helpers.CreateSecret(
+						client,
+						secretName,
+						map[string]string{
+							"key.pem":      string(pemKey),
+							"GH_CLIENT_ID": gitHubdeliveryClientId.GetText(),
+						},
+					)
+				}
 			}
 		}
 		// other delivery
@@ -65,27 +103,28 @@ func init() {
 		}
 	})
 	OutboundContainer.
-		AddItem(OutboundSettingsForm, 0, 2, true).
-		AddItem(GhIdpForm, 0, 4, false).
+		AddItem(OutboundSettingsForm, 0, 3, true).
+		AddItem(GhIdpForm, 0, 6, false).
+		AddItem(GhSecretsContainer, 0, 6, false).
 		AddItem(createGHSecretButton, 0, 2, false)
+	GhSecretsContainer.
+		AddItem(GhTriggerForm, 0, 1, true)
 }
 
 var OutboundContainer = tview.NewFlex().SetDirection(tview.FlexRow)
+var GhSecretsContainer = tview.NewFlex().SetDirection(tview.FlexColumn)
 
 func HandleDeliveryOpts(option string, optionIndex int) {
 	switch strings.ToLower(option) {
 	case "github":
-		OutboundContainer.RemoveItem(OtherDeliveryForm)
-		OutboundContainer.RemoveItem(createGHSecretButton)
-		OutboundContainer.AddItem(GhDeliveryForm, 0, 4, false)
-		OutboundContainer.AddItem(createGHSecretButton, 0, 1, false)
+		if !SameAsTrigger.IsChecked() {
+			GhSecretsContainer.AddItem(GhDeliveryForm, 0, 1, false)
+		}
 	case "other":
-		OutboundContainer.RemoveItem(GhDeliveryForm)
-		OutboundContainer.RemoveItem(createGHSecretButton)
-		OutboundContainer.AddItem(OtherDeliveryForm, 0, 4, false)
-		OutboundContainer.AddItem(createGHSecretButton, 0, 1, false)
+		GhSecretsContainer.RemoveItem(GhDeliveryForm)
+		GhSecretsContainer.AddItem(OtherDeliveryForm, 0, 1, false)
 	default:
-		OutboundContainer.RemoveItem(GhDeliveryForm)
-		OutboundContainer.RemoveItem(OtherDeliveryForm)
+		GhSecretsContainer.RemoveItem(GhDeliveryForm)
+		GhSecretsContainer.RemoveItem(OtherDeliveryForm)
 	}
 }
